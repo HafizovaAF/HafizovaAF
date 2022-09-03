3041acf2206c2fe620ab945bc3796cc2aa42e203
HafizovaAF <Hafizovaalina@mail.ru>
2022-09-03 12:18:37 +0700

    Завершение урока

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,14 @@
 ﻿using System;
 
-Console.WriteLine("Hello World!");
 
-  
+int x, y;
+Console.WriteLine("Введите x");
+ x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите y");
+y = Convert.ToInt32(Console.ReadLine());
+
+int z = x;
+y = x;
+x = y;
+
+Console.WriteLine($"x=(x),y=(y)");
